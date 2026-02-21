9e8af861f8a89317428a2d95de11fa74aeddb5bc
Акылай <osonovaakylai@gmail.com>
2026-02-21 13:09:52 +0600

    add public path

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = (env, argv) => {
       path: path.resolve(__dirname, 'dist'),
       filename: isDev ? 'bundle.js' : 'bundle.[contenthash].js',
       clean: true,
-      publicPath: isDev ? '/' : '/casino-royale',
+      publicPath: isDev ? '/' : '/casino-royale/',
     },
 
     resolve: {
